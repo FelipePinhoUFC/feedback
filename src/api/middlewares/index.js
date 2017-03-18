746e6840b3d726f5d1f134d737246405b639a631
Felipe Pinho <felipe76857685@gmail.com>
2017-03-18 11:08:59 -0300

    Fazendo login

--- src/api/middlewares/index.js
+++ src/api/middlewares/index.js
@@ -2,26 +2,46 @@ import actions from 'api/actions';
 import { push } from 'react-router-redux';
 
 export default (store) => {
-  return (dispatch) => {
-    return (action) => {
+    return (dispatch) => {
+        return (action) => {
 
-      if (action.type === actions.user.register) {
-        const { auth } = store.getState();
-        const user = auth.getIn(['emailSignIn', 'default', 'form']);
-        const { email, password } = user.toJS();
-        window.firebase
-          .auth()
-          .createUserWithEmailAndPassword(email, password)
-          .then(payload => {
-            store.dispatch(push('/login'));
-          })
-          .catch(payload => {
-            console.log(payload);
-            store.dispatch({ type: actions.user.registerFailure, payload })
-          });
-      }
+            if (action.type === actions.user.register) {
+                const { auth } = store.getState();
+                const user = auth.getIn(['emailSignIn', 'default', 'form']);
+                const { email, password } = user.toJS();
+                window.firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(email, password)
+                    .then(payload => {
+                        store.dispatch(push('/login'));
+                    })
+                    .catch(payload => {
+                        console.log(payload);
+                        store.dispatch({ type: actions.user.registerFailure, payload })
+                    });
+            }
 
-      return dispatch(action);
+            if (action.type === actions.user.login) {
+                const { auth } = store.getState();
+                const user = auth.getIn(['emailSignIn', 'default', 'form']);
+                const { email, password } = user.toJS();
+                window.firebase
+                    .auth()
+                    .signInWithEmailAndPassword(email, password)
+                    .then(payload => {
+                        store.dispatch({ type: actions.user.logged, payload: { user } })
+                    })
+                    .catch(payload => {
+                        console.log(payload);
+                        store.dispatch({ type: actions.user.loginFailure, payload })
+                    });
+            }
+
+            if (action.type === actions.user.logged) {
+                store.dispatch(push('/profile'));
+            }
+
+            return dispatch(action);
+        }
     }
-  }
 }
--- src/api/reducers/auth.js
+++ src/api/reducers/auth.js
@@ -2,43 +2,46 @@ import { fromJS } from 'immutable';
 import actions from 'api/actions';
 
 const initialState = fromJS({
-  authToken: {},
-  user: {
-    email: '',
-  },
-  emailSignIn: {
-    loading: {},
-    default: {
-      form: {
-        email: 'cmilfont@gmail.com',
-        password: 'testes55',
-        rememberMe: false,
-        recaptcha: false,
-      },
-      errors: {}
+    authToken: {},
+    user: {
+        email: '',
+    },
+    emailSignIn: {
+        loading: {},
+        default: {
+            form: {
+                email: '',
+                password: '',
+                rememberMe: false,
+                recaptcha: false,
+            },
+            errors: {}
+        }
     }
-  }
 
 });
 
 export default (state = initialState, action) => {
-  if (action.type === actions.user.loaded) {
-    return initialState;
-  }
-  if (action.type === actions.user.updateForm) {
-    return state.mergeDeepIn(['emailSignIn', 'default', 'form'], action.payload);
-  }
-  if (action.type === actions.user.logged) {
-    return state.merge(action.payload);
-  }
-  if (action.type === actions.user.loginFailure) {
-    return state.mergeDeepIn(['emailSignIn', 'default'], action.payload);
-  }
-  if (action.type === actions.user.registerFailure) {
-    const { code, message } = action.payload;
-    return state.mergeDeepIn(['emailSignIn', 'default', 'errors'], {
-      [code]: message,
-    });
-  }
-  return state;
+    if (action.type === actions.user.loaded) {
+        return initialState;
+    }
+    if (action.type === actions.user.updateForm) {
+        return state.mergeDeepIn(['emailSignIn', 'default', 'form'], action.payload);
+    }
+    if (action.type === actions.user.logged) {
+        return state.merge(action.payload);
+    }
+    if (action.type === actions.user.loginFailure) {
+        const { code, message } = action.payload;
+        return state.mergeDeepIn(['emailSignIn', 'default', 'errors'], {
+            [code]: message,
+        });
+    }
+    if (action.type === actions.user.registerFailure) {
+        const { code, message } = action.payload;
+        return state.mergeDeepIn(['emailSignIn', 'default', 'errors'], {
+            [code]: message,
+        });
+    }
+    return state;
 }
